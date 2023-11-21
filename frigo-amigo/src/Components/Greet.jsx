055e7d2458e36dcbe418bbae0a1f407ca93a4cda
Lizosoma <101399506+Lizosoma@users.noreply.github.com>
2023-11-21 20:44:08 +0300

    something

---
+++ frigo-amigo/src/Components/Greet.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+    <div className="wrapper">
+      <div className="greeting">
+        <p className="nextart-900 privet">Привет</p>
+        <p className="roundmplus-400 newld">Я FrigoAmigo - новый лучший друг твоего холодильника</p>
+        <p className="roundmplus-400 privet-info">Следить за состоянием продуктов теперь легче. Открывай свой холодильник, сканируй продукты, разделяй их по категориям и отслеживай срок годности </p>
+        <p className="nextart-400 stay-frash">Stay fresh</p>
+      </div>
+      <div className="form">
+
+      </div>
+    </div>
+  )
+}
+
+export default Greet
