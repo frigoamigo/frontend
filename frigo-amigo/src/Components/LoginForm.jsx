93d89891c6d439d09a4b6d1ad1b4d337cdf767d4
Lizosoma <101399506+Lizosoma@users.noreply.github.com>
2023-12-01 15:41:59 +0300

    reg validation, em, center

--- frigo-amigo/src/Components/LoginForm.jsx
+++ frigo-amigo/src/Components/LoginForm.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
     const { isLoginButtonClicked } = this.props;
+    const { email, password } = this.state;
+    const isButtonDisabled = email.trim() === '' || password.trim() === '';
+
     return (
       <form style={{ display: isLoginButtonClicked ? 'none' : 'flex' }}>
         <ul className='form-list flex'>
@@ -12,6 +29,8 @@ class LoginForm extends React.Component {
               className='form-input manrope-100'
               name='email'
               placeholder="Логин"
+              value={email}
+              onChange={this.handleInputChange}
             />
           </li>
           <li className='form-item'>
@@ -20,10 +39,16 @@ class LoginForm extends React.Component {
               className='form-input manrope-100'
               name='password'
               placeholder="Пароль"
+              value={password}
+              onChange={this.handleInputChange}
             />
           </li>
           <li className='form-item'>
-            <button className='form-btn manrope-100' type="button" onClick={this.props.toggleShift}>
+            <button className='form-btn manrope-100' 
+            type="button" 
+            onClick={this.props.toggleShift}
+            disabled={isButtonDisabled}
+            >
               <span>Войти</span>
             </button>
           </li>
