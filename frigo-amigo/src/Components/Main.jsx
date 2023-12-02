cad709849e9c444d75c7388b21efbb6d198bc8f3
Lizosoma <101399506+Lizosoma@users.noreply.github.com>
2023-12-02 22:35:36 +0300

    scroll added

--- frigo-amigo/src/Components/Main.jsx
+++ frigo-amigo/src/Components/Main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Greet from './Greet'
 import bg from "../Pictures/mainbg.png"
 
-const Main = () => {
+const Main = React.forwardRef((props, ref) => {
   return (
     <div className='main'>
       <div className='container'>
-        <img className='main_bg' alt='bg' src={bg}></img>
+        <img ref={ref} className='main_bg' alt='bg' src={bg}></img>
         <Greet />
       </div>
     </div>
-  )
-}
+  );
+});
 
 export default Main
--- frigo-amigo/src/Components/Premium.jsx
+++ frigo-amigo/src/Components/Premium.jsx
@@ -2,12 +2,13 @@ import PremiumCards from "./PremiumCards";
 import PremiumCircles from "./PremiumCircles";
 import PremiumHeading from "./PremiumHeading";
 import PremiumLowerText from "./PremiumLowerText";
+import React from 'react';
 
 
-const Premium = () => {
+const Premium = React.forwardRef((props, ref) => {
     return(
         <div className="container">
-        <div className="premium-block">
+        <div ref={ref} className="premium-block">
             <PremiumHeading />
             <PremiumCards />
             <PremiumCircles />
@@ -15,6 +16,6 @@ const Premium = () => {
         </div>
         </div>
     )
-}
+});
 
 export default Premium;
